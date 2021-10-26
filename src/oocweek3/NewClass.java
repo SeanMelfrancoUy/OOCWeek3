8bbd544a01746b4d52d83df29660855ce610e15a
SeanMelfrancoUy <72450766+SeanUy2001@users.noreply.github.com>
2021-10-26 16:40:23 +0100

    V2
    This is a test. Added a new class.

---
+++ src/oocweek3/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oocweek3;
+
+/**
+ *
+ * @author seanuy
+ */
+public class NewClass {
+    
+    public NewClass(){
+        System.out.println("New Class");
+    }
+    
+
+}
